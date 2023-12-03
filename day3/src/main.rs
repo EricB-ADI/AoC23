cb18394e33fcdc217d28dd58d5e135304e5dfa5d
Alpacabet <49300674+Eric-Busch0@users.noreply.github.com>
2023-12-03 16:51:51 -0600

    split parts into functions

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -15,81 +15,50 @@ fn read_lines_into_vector(file_path: &str) -> Result<Vec<String>, io::Error> {
 
     Ok(lines)
 }
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_path = match args.get(1){
-        Some(fp) => fp,
-        None => panic!("Expected file path of input!")
-    };
-    let lines: Vec<String> = read_lines_into_vector(&file_path).unwrap();
-
-    let mut symbol_locations: Vec<Vec<i32>> = Vec::new();
-    let mut possible_gears: Vec<Vec<usize>> = Vec::new();
-    let mut parsed_lines = vec![];
-    let re = Regex::new(r"(\d+)").unwrap();
-
-    for (i, line) in lines.iter().enumerate() {
-        symbol_locations.push(Vec::new());
-        possible_gears.push(Vec::new());
-
-        for (j, c) in line.chars().enumerate() {
-            if !c.is_numeric() && c != '.' {
-                symbol_locations[i].push(j as i32);
-                if c == '*' {
-                    possible_gears[i].push(j);
-                }
-            }
-        }
-        let parsed: Vec<(usize, usize, u32)> = re
-            .find_iter(line)
-            .map(|m| (m.start(), m.end(), m.as_str().parse().unwrap()))
-            .collect();
-        parsed_lines.push(parsed);
-    }
+fn part1(lines: &Vec<String>, parsed_lines: &Vec<Vec<(usize, usize, u32)>>, symbol_locations:&Vec<Vec<usize>>) -> u32{
 
     let mut nums = vec![];
 
-    for (line_num, line) in lines.iter().enumerate() {
-        let numbers_with_indices: Vec<(i32, i32, &str)> = re
-            .find_iter(line)
-            .map(|m| (m.start() as i32, m.end() as i32, m.as_str()))
-            .collect();
+    for (line_num, _) in lines.iter().enumerate() {
+        for (start, stop, num) in &parsed_lines[line_num] {
+            let leftbound = start.wrapping_sub(1);
+            let rightbound = *stop; // rust iterator is one more than the last index
+            let topbound = line_num.wrapping_sub(1);
+            let bottombound= line_num  + 1;
+            let num_parsed = *num;
 
-        for (start, stop, num) in numbers_with_indices {
-            let leftbound = start - 1;
-            let rightbound = stop; // rust iterator is one more than the last index
-            let topbound: i32 = line_num as i32 - 1;
-            let bottombound: i32 = line_num as i32 + 1;
-            let num_parsed = num.parse::<u32>().unwrap();
-
-            if symbol_locations[line_num].contains(&leftbound)
-                || symbol_locations[line_num].contains(&rightbound)
+            if symbol_locations[line_num]
+                .iter()
+                .any(|loc| *loc == leftbound || *loc == rightbound)
+            {
+                nums.push(num_parsed);
+                continue;
+            }
+            
+            if topbound < lines.len()
+                && symbol_locations[topbound]
+                    .iter()
+                    .any(|x| *x >= leftbound && *x <= rightbound)
             {
                 nums.push(num_parsed);
                 continue;
             }
 
-            if topbound >= 0 {
-                if symbol_locations[topbound as usize]
+            if bottombound < lines.len()
+                && symbol_locations[bottombound as usize]
                     .iter()
-                    .any(|x: &i32| x >= &leftbound && x <= &rightbound)
-                {
-                    nums.push(num_parsed);
-                    continue;
-                }
-            }
-
-            if bottombound < lines.len() as i32 {
-                if symbol_locations[bottombound as usize]
-                    .iter()
-                    .any(|x: &i32| x >= &leftbound && x <= &rightbound)
-                {
-                    nums.push(num_parsed);
-                }
+                    .any(|x| *x >= leftbound && *x <= rightbound)
+            {
+                nums.push(num_parsed);
             }
         }
     }
+
+
+    return  nums.iter().sum();
+}
+
+fn part2(possible_gears:Vec<Vec<usize>>, parsed_lines: &Vec<Vec<(usize, usize, u32)>>) -> u32{
 
     let mut gear_ratios = vec![];
 
@@ -133,6 +102,41 @@ fn main() {
         }
     }
 
-    println!("SUM1 {:?}", nums.iter().sum::<u32>());
-    println!("GEAR SUM {:?}", gear_ratios.iter().sum::<u32>());
+    return gear_ratios.iter().sum();
+}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let lines: Vec<String> = match args.get(1) {
+        Some(fp) => read_lines_into_vector(&fp).unwrap(),
+        None => panic!("Expected file path of input!"),
+    };
+    
+
+    let mut symbol_locations: Vec<Vec<usize>> = Vec::new();
+    let mut possible_gears: Vec<Vec<usize>> = Vec::new();
+    let mut parsed_lines: Vec<Vec<(usize, usize, u32)>> = vec![];
+    let re = Regex::new(r"(\d+)").unwrap();
+
+    for (i, line) in lines.iter().enumerate() {
+        symbol_locations.push(Vec::new());
+        possible_gears.push(Vec::new());
+
+        for (j, c) in line.chars().enumerate() {
+            if !c.is_numeric() && c != '.' {
+                symbol_locations[i].push(j);
+                if c == '*' {
+                    possible_gears[i].push(j);
+                }
+            }
+        }
+        let parsed: Vec<(usize, usize, u32)> = re
+            .find_iter(line)
+            .map(|m| (m.start(), m.end(), m.as_str().parse().unwrap()))
+            .collect();
+        parsed_lines.push(parsed);
+    }
+
+    println!("SUM1 {:?}", part1(&lines, &parsed_lines, &symbol_locations));
+    println!("GEAR SUM {:?}", part2(possible_gears, &parsed_lines));
 }
