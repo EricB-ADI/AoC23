8480b9436784102e05be51262226a8d84648464c
EricB-ADI <122300463+EricB-ADI@users.noreply.github.com>
2023-12-04 15:23:40 -0600

    build in release much much faster

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,3 @@
-use core::num;
-use std::collections::HashMap;
-use regex::Regex;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -18,47 +15,42 @@ fn read_lines_into_vector(file_path: &str) -> Result<Vec<String>, io::Error> {
     Ok(lines)
 }
 
-fn solve_pls(indicies: &Vec<usize>, all: &Vec<String>) ->u32
-{
-    
-    if indicies.is_empty(){ return 0};
+fn solve_pls(indicies: &Vec<usize>, all: &Vec<String>) -> u32 {
+    if indicies.is_empty() {
+        return 0;
+    };
 
     let mut total = 0;
-    for index in indicies{
-
+    for index in indicies {
         let card = all[*index].clone();
 
         let ginfo: Vec<&str> = card.split(":").collect();
-            let card_num = ginfo
+        let card_num = ginfo
             .get(0)
             .unwrap()
-                .split_whitespace()
-                .nth(1)
-                .unwrap().parse::<u32>().unwrap();
-    
-            let mut card_copies = vec![];
-            let nums: Vec<&str> = ginfo.get(1).unwrap().split("|").collect();
-            let winning: Vec<&str> = nums[0].split_whitespace().collect();
-            let your_nums: Vec<&str> = nums[1].split_whitespace().collect();
-            let mut winners:u32 = 0;
-            for num in winning {
-                if your_nums.contains(&num) {
-                    winners += 1;
-                    card_copies.push(winners as usize + card_num as usize - 1 as usize);
-                
-                }
+            .split_whitespace()
+            .nth(1)
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+
+        let mut card_copies = vec![];
+        let nums: Vec<&str> = ginfo.get(1).unwrap().split("|").collect();
+        let winning: Vec<&str> = nums[0].split_whitespace().collect();
+        let your_nums: Vec<&str> = nums[1].split_whitespace().collect();
+        let mut winners: u32 = 0;
+        for num in winning {
+            if your_nums.contains(&num) {
+                winners += 1;
+                card_copies.push(winners as usize + card_num as usize - 1 as usize);
             }
-            total += winners;
+        }
+        total += winners;
 
-                
-            total += solve_pls(&card_copies,all);
-            
-
+        total += solve_pls(&card_copies, all);
     }
     return total;
-
 }
-
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -89,11 +81,8 @@ fn main() {
         worth += game_worth;
     }
 
-
     let init_idx: Vec<usize> = (0..lines.len()).collect();
-    
+
     println!("{}", worth);
     println!("{}", lines.len() as u32 + solve_pls(&init_idx, &lines));
-
-    
 }
