364c0004c867939235c8478f3a1fd61fc00dde99
Alpacabet <49300674+Eric-Busch0@users.noreply.github.com>
2023-12-04 20:21:02 -0600

    got better solution

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,4 @@
+use core::time;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -14,44 +15,6 @@ fn read_lines_into_vector(file_path: &str) -> Result<Vec<String>, io::Error> {
 
     Ok(lines)
 }
-
-fn solve_pls(indicies: &Vec<usize>, all: &Vec<String>) -> u32 {
-    if indicies.is_empty() {
-        return 0;
-    };
-
-    let mut total = 0;
-    for index in indicies {
-        let card = all[*index].clone();
-
-        let ginfo: Vec<&str> = card.split(":").collect();
-        let card_num = ginfo
-            .get(0)
-            .unwrap()
-            .split_whitespace()
-            .nth(1)
-            .unwrap()
-            .parse::<u32>()
-            .unwrap();
-
-        let mut card_copies = vec![];
-        let nums: Vec<&str> = ginfo.get(1).unwrap().split("|").collect();
-        let winning: Vec<&str> = nums[0].split_whitespace().collect();
-        let your_nums: Vec<&str> = nums[1].split_whitespace().collect();
-        let mut winners: u32 = 0;
-        for num in winning {
-            if your_nums.contains(&num) {
-                winners += 1;
-                card_copies.push(winners as usize + card_num as usize - 1 as usize);
-            }
-        }
-        total += winners;
-
-        total += solve_pls(&card_copies, all);
-    }
-    return total;
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -62,15 +25,19 @@ fn main() {
 
     let mut worth = 0;
 
-    for line in &lines {
+    let mut times_played = vec![1;lines.len()];
+
+    for (game_num, line) in lines.iter().enumerate() {
         let ginfo: Vec<&str> = line.split(":").collect();
         let nums: Vec<&str> = ginfo.get(1).unwrap().split("|").collect();
         let winning: Vec<&str> = nums[0].split_whitespace().collect();
         let your_nums: Vec<&str> = nums[1].split_whitespace().collect();
 
         let mut game_worth = 0;
+        let mut total_matches = 0;
         for num in winning {
             if your_nums.contains(&num) {
+                total_matches +=1;
                 if game_worth == 0 {
                     game_worth = 1;
                 } else {
@@ -79,10 +46,22 @@ fn main() {
             }
         }
         worth += game_worth;
+        
+        
+        for w in 0 .. total_matches
+        {
+            times_played[game_num + w +1] += times_played[game_num];
+        }
+    
     }
+
+
+
 
     let init_idx: Vec<usize> = (0..lines.len()).collect();
 
     println!("{}", worth);
-    println!("{}", lines.len() as u32 + solve_pls(&init_idx, &lines));
+    println!("{}", times_played.iter().sum::<i32>());
+
+    // println!("{}", lines.len() as u32 + solve_pls(&init_idx, &lines));
 }
